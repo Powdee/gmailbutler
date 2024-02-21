9615a80ee14d96273c72c7730d9f52c6d1f9d972
Erik Kurjak <e.kurjak@gmail.com>
2024-02-21 15:33:57 +0100

    update shell script

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Build the Go program
 go build -o build/gmailbutler
 
-# Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Build successful, running the program..."
-    # Run the built program
     ./build/gmailbutler
 else
     echo "Build failed."
